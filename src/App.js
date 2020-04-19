55584debe775e653ac1a246d76c8113e8dec390e
Alok Raj <alokr417@gmail.com>
2020-04-19 16:25:25 +0530

    Passing argument to method using bind

--- src/App.js
+++ src/App.js
@@ -12,13 +12,11 @@ class App extends Component {
     otherState: 'Some other values'
   }
 
-  switchNameHandler = () => {
-    // console.log('Clicked !!!')
-    // Don't do this: this.state.persons[0].firstName = 'Alok Raj';
+  switchNameHandler = (newName) => {
     this.setState({persons : [
       {firstName : 'Alok Raj', secondName : 'Harshita Kumari'},
       {firstName : 'Harshita Kumari', secondName : 'Alok Raj'},
-      {firstName : 'AA', secondName : 'BB'}
+      {firstName : newName, secondName : 'BB'}
     ]}) //This won't affect otherState defined inside state.
   }
 
@@ -26,7 +24,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello, There !!!</h1>
-        <button onClick = {this.switchNameHandler}>Switch Name</button>
+        <button onClick = {this.switchNameHandler.bind(this, 'AA')}>Switch Name</button>
         <Person 
           firstName = {this.state.persons[0].firstName} 
           secondName = {this.state.persons[0].secondName}/>
@@ -36,7 +34,7 @@ class App extends Component {
         <Person 
           firstName = {this.state.persons[2].firstName} 
           secondName = {this.state.persons[2].secondName}
-          click = {this.switchNameHandler}
+          click = {this.switchNameHandler.bind(this, 'AA!')}
           >This is a child prop.</Person>
       </div>
     );
