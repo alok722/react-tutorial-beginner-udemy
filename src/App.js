a18fcb10ef78e97be6b4be53408425491825c519
Alok Raj <alokr417@gmail.com>
2020-04-20 00:51:40 +0530

    Deletion from Array with key flaw

--- src/App.js
+++ src/App.js
@@ -13,16 +13,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: 'Alok Raj', age: 22 },
-        { name: 'Ashish Raj', age: 20 },
-        { name: newName, age: 18 }
-      ]
-    }) //This won't affect otherState defined inside state.
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -31,6 +21,12 @@ class App extends Component {
         { name: event.target.value, age: 18 }
       ]
     })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   togglePersonHandler = () => {
@@ -50,8 +46,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person,index) => {
             return <Person 
+              click = { () => this.deletePersonHandler(index)}
               name = {person.name}
               age = {person.age}/>
           })}
